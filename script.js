3f3b2a3c09c74e8ee3321aaa6e5c20de16c58987
Jash <128972302+Jashbaua@users.noreply.github.com>
2024-08-29 20:16:39 +0530

    Add button to change resolution

--- script.js
+++ script.js
@@ -8,10 +8,26 @@ function createGrid(n){
         box.classList.add("box")
         cnt.appendChild(box)
     }
+    const boxes=document.querySelectorAll('.box')
+    boxes.forEach((box)=>box.addEventListener('mouseenter',changeColor))
+    function changeColor(event){
+        event.target.style.backgroundColor="black"
+    }
 }
 createGrid(16)
-const boxes=document.querySelectorAll('.box')
-boxes.forEach((box)=>box.addEventListener('mouseenter',changeColor))
-function changeColor(event){
-    event.target.style.backgroundColor="black"
+const btn=document.querySelector('button')
+btn.addEventListener('click',promptForResolution)
+function deletegrid(){
+    const cnt=document.querySelector('.container')
+    cnt.textContent=''
+}
+function promptForResolution(){
+    let numberOfBoxesPerSide=prompt("Max limit: 100")
+    if(numberOfBoxesPerSide>100){
+        alert("Enter number less than 100!")
+    }
+    else {
+        deletegrid()
+        createGrid(numberOfBoxesPerSide)
+    }
 }
